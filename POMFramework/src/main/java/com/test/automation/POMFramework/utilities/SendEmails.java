514ce92c7c30105c9ff5e61f6ddef6d8fe31143e
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-05-09 13:07:03 -0500

    Update SendEmails.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/utilities/SendEmails.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/utilities/SendEmails.java
@@ -17,25 +17,50 @@ http://learn-automation.com/send-report-through-email-in-selenium-webdriver/
 
 public class SendEmails {
 
-	public static void sendSimleEmail()   {
-
-		Email email = new SimpleEmail();
-		email.setHostName("smtp.gmail.com");
-		email.setSmtpPort(465);
-		email.setAuthenticator(new DefaultAuthenticator("username", "password"));
-		email.setSSLOnConnect(true);
-		try {
-			email.setFrom("eniolaoludare@gmail.com");
-			email.setSubject("Oh, just found a bug that needs to be fixed");	
-			email.setMsg(("Hello, kindly check the test result with the screenshot and help fix the bug ... :-). \n Test execution report folder below: \n") +
-					("C:\\Users\\Da Novenos\\eclipse-workspace\\POMFramework\\test-output\\ExtentHtmlReport")  + "\n" + "Thanks" );
-			email.addTo("eniola.oludare@yahoo.com");
-			email.send();
-			System.out.println("Email sent");	
-		} catch (EmailException e) {
-			System.out.println("Connection error occurred: Email could not be sent!");;
-		}
-	}
+	public static void sendEmai(String filename, String emailReciever){
+	
+			Properties props = new Properties();
+			// this will set host of server- you can change based on your requirement 
+			props.put("mail.smtp.host", "smtp.stls.frb.org");
+			props.put("mail.smtp.socketFactory.port", "465");
+			props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.port", "465");
+			// This will handle the complete authentication
+			Session session = Session.getDefaultInstance(props,
+					new javax.mail.Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("Olu.Eniola@stls.frb.org", "my window password");
+						}
+					});
+			try {
+				// Create object of MimeMessage class
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("Olu.Eniola@stls.frb.org"));
+				message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(emailReciever));
+				message.setSubject("Oh, bug(s) detected and need(s) to be fixed");
+				// Create object to add Multi-media type content
+				BodyPart messageBodyPart1 = new MimeBodyPart();
+				// Set the body of email
+				messageBodyPart1.setText(("Hello, kindly check the test result with the screenshot and help fix the bug ... :-). \n Test execution report folder below: \n") +
+						("path to testOut \\ExtentHtmlReport")  + "\n" + "Thanks");
+				// Create another object to add another content
+				MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+				// Create data source and pass the filename
+				DataSource source = new FileDataSource(filename);
+				messageBodyPart2.setDataHandler(new DataHandler(source));
+				messageBodyPart2.setFileName(filename);
+				// Create object of MimeMultipart class
+				Multipart multipart = new MimeMultipart();
+				multipart.addBodyPart(messageBodyPart2);
+				multipart.addBodyPart(messageBodyPart1);
+				message.setContent(multipart);
+				Transport.send(message);
+				System.out.println("=====Email Sent=====");
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
+			}
+}
 
 	
 	public static void sendEmailWithAttachment() throws EmailException  {
