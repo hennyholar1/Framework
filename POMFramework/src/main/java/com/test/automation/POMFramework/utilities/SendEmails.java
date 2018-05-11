da17ce58641aaa14676b79a99876b855086edd06
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-05-11 17:30:01 -0500

    Update SendEmails.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/utilities/SendEmails.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/utilities/SendEmails.java
@@ -1,21 +1,18 @@
 package com.test.automation.POMFramework.utilities;
 
+// package utilities;
+
 import java.util.*;
 import javax.activation.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
 
-/**
-// Useful url for email sending
-http://www.assertselenium.com/java/emailable-reports-for-selenium-scripts/
-http://learn-automation.com/send-report-through-email-in-selenium-webdriver/
-*/
-
 public class SendEmails {
-
-	public static void sendEmai(String filename, String emailReciever){
 	
+	public static void sendEmai(String pathToFileToBeAttached, String emailReciever){
+	
+		// I need smtp host, port, socketFactory port information 
 			Properties props = new Properties();
 			// this will set host of server- you can change based on your requirement 
 			props.put("mail.smtp.host", "smtp.stls.frb.org");
@@ -39,14 +36,17 @@ public class SendEmails {
 				// Create object to add Multi-media type content
 				BodyPart messageBodyPart1 = new MimeBodyPart();
 				// Set the body of email
-				messageBodyPart1.setText(("Hello, kindly check the test result with the screenshot and help fix the bug ... :-). \n Test execution report folder below: \n") +
-						("path to testOut \\ExtentHtmlReport")  + "\n" + "Thanks");
+				messageBodyPart1.setText(
+						("Hello, kindly check the test result/report with the screenshot and help fix the bug ... :-). "
+						+ "\n Test execution report folder below: \n") 
+						+ ("path to testOut \\ExtentHtmlReport")  + "\n" + "Thanks");
+				
 				// Create another object to add another content
 				MimeBodyPart messageBodyPart2 = new MimeBodyPart();
 				// Create data source and pass the filename
-				DataSource source = new FileDataSource(filename);
+				DataSource source = new FileDataSource(pathToFileToBeAttached);
 				messageBodyPart2.setDataHandler(new DataHandler(source));
-				messageBodyPart2.setFileName(filename);
+				messageBodyPart2.setFileName(pathToFileToBeAttached);
 				// Create object of MimeMultipart class
 				Multipart multipart = new MimeMultipart();
 				multipart.addBodyPart(messageBodyPart2);
@@ -60,54 +60,88 @@ public class SendEmails {
 }
 
 	
-	public static void sendEmailWithAttachment() throws EmailException  {
+	
+	// supply your SMTP host name
+		private static String host = "";	
+		private static String to = "";
+		private static String from = "";
+		private static String cc = "";
+		private static String bcc = "";
 
-		// Create the attachment
-		EmailAttachment attachment = new EmailAttachment();
-		attachment.setPath("mypictures/john.jpg");
-		attachment.setDisposition(EmailAttachment.ATTACHMENT);
-		attachment.setDescription("Picture of John");
-		attachment.setName("John");
+		
+	public static void sendEmai(String from, String to, String subject, String contents) throws MessagingException {		
+		
+		Properties prop = System.getProperties();
+		prop.setProperty("mail.smtp.host", host);
+		
+		Session session = Session.getDefaultInstance(prop);
+		
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+		message.setSubject(subject);
+		message.setContent(contents, "text/html");
 
-		// Create the email message
-		MultiPartEmail email = new MultiPartEmail();
-		email.setHostName("mail.myserver.com");			// Get server SMTP
-		email.addTo("jdoe@somewhere.org", "John Doe");	// Get the port number
-		email.setFrom("me@apache.org", "Me");
-		email.setSubject("The picture");
-		email.setMsg("Here is the picture you wanted");
-
-		// add the attachment
-		email.attach(attachment);
-
-		// send the email
-		email.send();
-
-	}
+		List<String> toList = getAddress(to);
+		for (String address : toList) {
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(address));
+		}
+		
+		List<String> ccList = getAddress(cc);
+		for (String address : ccList) {
+			message.addRecipient(Message.RecipientType.CC, new InternetAddress(address));
+		}
+		
+		List<String> bccList = getAddress(bcc);
+		for (String address : bccList) {
+			message.addRecipient(Message.RecipientType.BCC, new InternetAddress(address));
+		}
+		
+		Transport.send(message);
+		}
+		
+		public static void sendEmai(String to, String subject, String contents) throws MessagingException {
+			sendEmai(from, to, subject, contents);
+		}
+		
+		public static void sendMai(String subject, String contents) throws MessagingException {
+			sendEmai(from, to, subject, contents);
+		}
+		
+		private static List getAddress(String address) {
+			List addressList = new ArrayList();
+			
+			if (address.isEmpty())
+				return addressList;
+			
+			if (address.indexOf(";") > 0) {
+				String[] addresses = address.split(";");
+				
+				for (String a : addresses) {
+					addressList.add(a);
+				}
+			} else {
+				addressList.add(address);
+			}
+			
+			return addressList;
+		}
 
 	
-	public static void emailWithReferenceAttachment() throws EmailException, MalformedURLException {
-		
-		// Create the attachment
-		EmailAttachment attachment = new EmailAttachment();
-		attachment.setURL(new URL("http://www.apache.org/images/asf_logo_wide.gif"));
-		attachment.setDisposition(EmailAttachment.ATTACHMENT);
-		attachment.setDescription("Apache logo");
-		attachment.setName("Apache logo");
+	
+/*
+public static void sendEmail(String message, String testCaseName)
+{
+MailMessage mail = new MailMessage();
+mail.To.Add("your-to-email-address-goes-here");
+mail.From = new MailAddress("your-from-email-address-goes-here ");
+mail.Subject = "your-mail-subject-goes-here";
+mail.Body = "Test Case Name: " + testCaseName;
+mail.Body += Environment.NewLine;
+mail.Body += message;
+SmtpClient smtp = new SmtpClient();
+smtp.Host = "localhost";
+smtp.Port = 25;
+smtp.Send(mail);
+}*/
 
-		// Create the email message
-		MultiPartEmail email = new MultiPartEmail();
-		email.setHostName("mail.myserver.com");
-		email.addTo("jdoe@somewhere.org", "John Doe");
-		email.setFrom("me@apache.org", "Me");
-		email.setSubject("The logo");
-		email.setMsg("Here is Apache's logo");
-
-		// add the attachment
-		email.attach(attachment);
-
-		// send the email
-		email.send();
-
-	}
-}
+		}
