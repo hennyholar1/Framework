a767c617047ead12408449b9eeaa54bc04b91627
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-06-04 09:38:58 -0500

    Update ResourceHelper.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/fileReader/ResourceHelper.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/fileReader/ResourceHelper.java
@@ -1,18 +1,42 @@
 package com.test.automation.POMFramework.fileReader;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class ResourceHelper {
-	
-	public static String getResourcePath(String resource){
-	
-		return System.getProperty("user.dir") +resource;
+
+	/**
+	 * This method will return resource path / project location irrespective of
+	 * driver location
+	 * 
+	 * @param resource
+	 * @param path
+	 * @return
+	 */
+
+	public static String getResourcePath(String resource) {
+		String getRelativePath = System.getProperty("user.dir");
+
+		return getRelativePath + resource;
 	}
+
 	
-	public static InputStream getResoucePathInputStream(String path) throws FileNotFoundException {
+	public static File getResourcePath(File resource) {
+
+		return resource;
+	}
+
 	
+	public static InputStream getResourcePathInputStream(String path) throws FileNotFoundException {
+
+		return new FileInputStream(ResourceHelper.getResourcePath(path));
+	}
+
+	
+	public static InputStream getResourcePathInputStream(File path) throws FileNotFoundException {
+
 		return new FileInputStream(ResourceHelper.getResourcePath(path));
 	}
 }
