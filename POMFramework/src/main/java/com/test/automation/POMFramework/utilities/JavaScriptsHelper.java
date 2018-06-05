6d988fcc33b4aeb0f8020b05b3cd77f54cdcd099
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-06-05 12:28:05 -0500

    Update JavaScriptsHelper.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/utilities/JavaScriptsHelper.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/utilities/JavaScriptsHelper.java
@@ -1,6 +1,4 @@
-
 package com.test.automation.POMFramework.utilities;
-
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -9,135 +7,97 @@ import org.openqa.selenium.WebElement;
 
 public class JavaScriptsHelper {
 
-	static WebDriver driver;
-	static JavascriptExecutor js = (JavascriptExecutor) driver;
+	static WebDriver dr;
 	
 	public JavaScriptsHelper(WebDriver driver) {
 		super();
-		this.driver = driver;
+		dr = driver;
 	}
 
+	
+	public static Object executeScript(String script) {			
 
+		return ((JavascriptExecutor) dr).executeScript(script);	
+	}
+
+	public static Object executeScript(String script, Object... args) {
+
+		return ((JavascriptExecutor) dr).executeScript(script, args);	
+	}
+	
+	
 	public static void highlightMe(WebDriver driver, WebElement element)  {		
 
-			
-
-		js.executeScript("arguments[0].style.border='4px solid yellow'", element);	
-
+		executeScript("arguments[0].style.border='4px solid yellow'", element);	
 		try {
-
 			Thread.sleep(3000);			
-
 		} catch (InterruptedException e) {
-
 						e.printStackTrace();
-
 		}		
-
-		js.executeScript("arguments[0].style.border=''", element);	
+		executeScript("arguments[0].style.border=''", element);	
 	}
-
-	
-
-	
 
 	public static void highlightElement(WebDriver driver, WebElement element)  {		
 
-		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);	
-
-		try {
-			
+		executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);	
+		try {			
 			Thread.sleep(3000);	
-
 		} catch (InterruptedException e) {
-
 			e.printStackTrace();
-
 		}		
-
-		js.executeScript("arguments[0].style.border=''", element);	
+		executeScript("arguments[0].style.border=''", element);	
 	}
-
-
-	public Object executeScript(String script) {			
-
-		return js.executeScript(script);	
-	}
-
-
-	public Object executeScript(String script, Object... args) {
-
-		return js.executeScript(script, args);	
-	}
-	
 
 	public void scrollToElement(WebElement element) {	
 
 		executeScript("window.scrollTo(arguments[0],arguments[1])", element.getLocation().x, element.getLocation().y);
 	}
 
-
 	public void scrollToElemetAndClick(WebElement element) {		
 
 		scrollToElement(element);		
-
 		element.click();	
 	}
-
 
 	public void scrollIntoView(WebElement element) {	
 
 		executeScript("arguments[0].scrollIntoView()", element);	
 	}
 	
-
 	public void scrollIntoViewAndClick(WebElement element) {
 
 		scrollIntoView(element);	
-
 		element.click();	
 	}
 	
-
-	public void scrollDownVertically() {	
+	public void scrollDown() {	
 
 		executeScript("window.scrollTo(0, document.body.scrollHeight)");	
 	}
 
-
-	public void scrollUpVertically() {	
+	public void scrollUp() {	
 
 		executeScript("window.scrollTo(0, -document.body.scrollHeight)");	
 	}
 	
+	public void scrollDownByPixel(int enterZeroValueHere, int enterPositiveValueHere) {	
 
-	public void scrollDownByPixel() {	
-
-		executeScript("window.scrollBy(0,1500)");	
+		executeScript("window.scrollBy("+ enterZeroValueHere +","+ enterPositiveValueHere + ")");	
 	}
 
+	public void scrollUpByPixel(int enterZeroValueHere, int enterNegativeValueHere) {	
 
-	public void scrollUpByPixel() {	
-
-		executeScript("window.scrollBy(0,-1500)");	
-	}
-
-	
-	public void ZoomInBypercentage() {
-
-		executeScript("document.body.style.zoom='40%'");
-	}
-
-	
-	public void ZoomBy100percentage() {	
-
-		executeScript("document.body.style.zoom='100%'");
+		executeScript("window.scrollBy("+ enterZeroValueHere +","+ enterNegativeValueHere + ")");	
 	}
 	
-	
+	public void ZoomInByPercentage(int percentageVale) {
+
+		executeScript("document.body.style.zoom='" + percentageVale + " %'");
+	}
+
 	public String getDisplayedValue(WebElement value) {
        
-        return (String) js.executeScript("return $('value').val();");
+        return (String)executeScript("return $('value').val();");
     }
 	
 }
