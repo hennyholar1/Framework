f2b7d82cfc06e94a887ea9365a185d04391088d3
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-06-05 11:20:33 -0500

    Update WaitHelper.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/utilities/WaitHelper.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/utilities/WaitHelper.java
@@ -1,9 +1,6 @@
 package com.test.automation.POMFramework.utilities;
 
-package utilities;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.NoSuchElementException;
@@ -18,22 +15,24 @@ import com.google.common.base.Function;
 
 public class WaitHelper {
 
-	private  WebDriver driver;
-	public WebDriverWait wait;
+	private static WebDriver driver;
+	private static WebDriverWait wait;
 
 
 	public WaitHelper(WebDriver driver, long timeOutInSeconds) {
+
 		super();
 		this.driver = driver;
-		wait = new WebDriverWait(driver, timeOutInSeconds);
+		this.wait = new WebDriverWait(driver, timeOutInSeconds);
 	}
 	
 	public WaitHelper(WebDriver driver) {
 
 		super();
 		this.driver = driver;
-		wait = new WebDriverWait(driver, 10);
+		this.wait = new WebDriverWait(driver, 10);
 	}
+
 	/**
 	 * This method will wait for element
 	 * @param element
@@ -43,9 +42,9 @@ public class WaitHelper {
 	
 
 	
-	public void fluentWait(WebElement element) {
+	public static void fluentWait(WebElement element) {
 
-		wait = (WebDriverWait) new FluentWait<WebDriver>(driver).withTimeout(20, TimeUnit.SECONDS)
+		wait = (WebDriverWait) new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS)
 				.pollingEvery(2, TimeUnit.SECONDS).ignoring(org.openqa.selenium.NoSuchElementException.class);
 		wait.until(new Function<WebDriver, WebElement>() {
 			public WebElement apply(WebDriver driver) {
@@ -55,7 +54,7 @@ public class WaitHelper {
 	}
 
 
-	public void waitForElement(WebElement element) {
+	public static WebElement waitForElement(WebElement element) {
 		
 		wait.ignoring(NoSuchElementException.class);
 		wait.ignoring(ElementNotVisibleException.class);
@@ -63,7 +62,8 @@ public class WaitHelper {
 		wait.ignoring(ElementNotFoundException.class);
 		wait.pollingEvery(200, TimeUnit.MILLISECONDS);
 		wait.until(ExpectedConditions.visibilityOf(element));
-		wait.until(elementLocated(element));		
+		wait.until(elementLocated(element));	
+		return element;
 	}
 	
 
@@ -76,7 +76,7 @@ public class WaitHelper {
 	}
 
 
-	public void waitImplicitly(long timeout) {
+	public static void waitImplicitly(long timeout) {
 
 		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
 	}
