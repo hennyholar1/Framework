35010820b04771106657d969616e26dc9d5d89f0
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-06-12 12:26:32 -0500

    Update WaitHelper.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/utilities/WaitHelper.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/utilities/WaitHelper.java
@@ -1,72 +1,78 @@
 package com.test.automation.POMFramework.utilities;
 
+package utilities;
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotVisibleException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.google.common.base.Function;
 
 public class WaitHelper {
 
 	private static WebDriver driver;
-	private static WebDriverWait wait;
-
-
-	public WaitHelper(WebDriver driver, long timeOutInSeconds) {
-
-		super();
-		this.driver = driver;
-		this.wait = new WebDriverWait(driver, timeOutInSeconds);
-	}
-	
-	public WaitHelper(WebDriver driver) {
-
-		super();
-		this.driver = driver;
-		this.wait = new WebDriverWait(driver, 10);
-	}
+	public static Wait<WebDriver> wait;
+//	public static WebDriverWait wait;
 
 	/**
 	 * This method will wait for element
 	 * @param element
-	 * @param time
 	 * @return 
 	 */
 	
 
+	public static void sleep(int timeInSecond){
+		
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	public static void fluentWait(WebElement element) {
+	public static WebElement waitForElementFluently(WebElement element) {
 
-		wait = (WebDriverWait) new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS)
-				.pollingEvery(2, TimeUnit.SECONDS).ignoring(org.openqa.selenium.NoSuchElementException.class);
-		wait.until(new Function<WebDriver, WebElement>() {
-			public WebElement apply(WebDriver driver) {
-				return driver.findElement(By.xpath("//*[contains(text(),'" + element + "')]"));
-			}
-		});
+		wait = new FluentWait<WebDriver>(driver)
+			.withTimeout(Duration.ofSeconds(15))
+            .pollingEvery(Duration.ofSeconds(1))
+            .ignoring(Exception.class);  
+		 return element;
+	}
+	
+	public static WebElement waitForElementFluently(String element) {
+
+		  wait = new FluentWait<WebDriver>(driver)
+			.withTimeout(Duration.ofSeconds(15))
+            .pollingEvery(Duration.ofSeconds(1))
+            .ignoring(Exception.class);
+		  return driver.findElement(By.xpath("//*[contains(text(),'" + element + "')]"));
 	}
 
 
 	public static WebElement waitForElement(WebElement element) {
-		
-		wait.ignoring(NoSuchElementException.class);
-		wait.ignoring(ElementNotVisibleException.class);
-		wait.ignoring(StaleElementReferenceException.class);
-		wait.ignoring(ElementNotFoundException.class);
-		wait.pollingEvery(200, TimeUnit.MILLISECONDS);
+		wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions.visibilityOf(element));
-		wait.until(elementLocated(element));	
-		return element;
-	}
+		wait.until(elementLocated(element));
+		wait.until(ExpectedConditions.presenceOfElementLocated((By) element));
+		return element;	}
 	
-
+	
+	public static WebElement waitForElement(String element) {
+		
+		wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOf 
+				(driver.findElement(By.xpath("//*[contains(text(),'" + element + "')]"))));
+		wait.until(elementLocated((driver.findElement(By.xpath("//*[contains(text(),'" + element + "')]")))));
+		wait.until(ExpectedConditions.presenceOfElementLocated((By) 
+				(driver.findElement(By.xpath("//*[contains(text(),'" + element + "')]")))));
+		 return driver.findElement(By.xpath("//*[contains(text(),'" + element + "')]"));
+		}
+	
 	private static Function<WebDriver, Boolean> elementLocated(final WebElement element) {
 		return new Function<WebDriver, Boolean>() {
 			public Boolean apply(WebDriver driver) {
