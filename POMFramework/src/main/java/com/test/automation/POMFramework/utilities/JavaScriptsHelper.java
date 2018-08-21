6b7187bfd79c6b2d4b039343933430fc32de99b5
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-08-21 10:07:54 -0500

    Update JavaScriptsHelper.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/utilities/JavaScriptsHelper.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/utilities/JavaScriptsHelper.java
@@ -1,103 +1,99 @@
 package com.test.automation.POMFramework.utilities;
 
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 
-public class JavaScriptsHelper {
+public class JavaScriptFunctions {
 
-	static WebDriver dr;
+	private static WebDriver myDriver;
+	JavascriptExecutor executor;
 	
-	public JavaScriptsHelper(WebDriver driver) {
-		super();
-		dr = driver;
+	public JavaScriptFunctions(WebDriver driver) {
+		myDriver = driver;
+		executor = ((JavascriptExecutor) myDriver);
 	}
 
+
+	public String getDisplayedValue(WebElement value) {
+        return (String) executor.executeScript("return $('" + value +" ').val();");
+    }
 	
-	public static Object executeScript(String script) {			
-
-		return ((JavascriptExecutor) dr).executeScript(script);	
-	}
-
-	public static Object executeScript(String script, Object... args) {
-
-		return ((JavascriptExecutor) dr).executeScript(script, args);	
+	public void clickOnElement(WebElement element){
+		executor.executeScript("arguments[0].click();", element);
 	}
 	
-	
-	public static void highlightMe(WebDriver driver, WebElement element)  {		
-
-		executeScript("arguments[0].style.border='4px solid yellow'", element);	
-		try {
-			Thread.sleep(3000);			
-		} catch (InterruptedException e) {
-						e.printStackTrace();
-		}		
-		executeScript("arguments[0].style.border=''", element);	
+	public void highlightElement(WebElement uiElement, String color)  {		
+		executor.executeScript(
+				"arguments[0].setAttribute('style', 'background: lava; border: 3px solid " + color + ";');", uiElement);
+		WaitFunctions.sleep(1);
+		executor.executeScript("arguments[0].setAttribute('style', arguments[1]);", uiElement, "");
 	}
-
-	public static void highlightElement(WebDriver driver, WebElement element)  {		
-
-		executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);	
-		try {			
-			Thread.sleep(3000);	
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}		
-		executeScript("arguments[0].style.border=''", element);	
+	
+	public void highlightElement(WebElement element)  {	
+		executor.executeScript(
+				"arguments[0].setAttribute('style', 'background: lava; border: 3px solid red;');", element);
+		WaitFunctions.sleep(1);
+		executor.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "");	
 	}
 
 	public void scrollToElement(WebElement element) {	
-
-		executeScript("window.scrollTo(arguments[0],arguments[1])", element.getLocation().x, element.getLocation().y);
+		executor.executeScript("window.scrollTo(arguments[0],arguments[1])", element.getLocation().x, element.getLocation().y);
 	}
 
 	public void scrollToElemetAndClick(WebElement element) {		
-
 		scrollToElement(element);		
 		element.click();	
 	}
 
 	public void scrollIntoView(WebElement element) {	
-
-		executeScript("arguments[0].scrollIntoView()", element);	
+		executor.executeScript("arguments[0].scrollIntoView()", element);	
 	}
 	
 	public void scrollIntoViewAndClick(WebElement element) {
-
 		scrollIntoView(element);	
 		element.click();	
 	}
 	
 	public void scrollDown() {	
-
-		executeScript("window.scrollTo(0, document.body.scrollHeight)");	
+		executor.executeScript("window.scrollTo(0, document.body.scrollHeight)");	
 	}
 
 	public void scrollUp() {	
-
-		executeScript("window.scrollTo(0, -document.body.scrollHeight)");	
+		executor.executeScript("window.scrollTo(0, -document.body.scrollHeight)");	
 	}
 	
-	public void scrollDownByPixel(int enterZeroValueHere, int enterPositiveValueHere) {	
-
-		executeScript("window.scrollBy("+ enterZeroValueHere +","+ enterPositiveValueHere + ")");	
+	public void scrollDownByPixel(int enterIntegerValueHere) {	
+		executor.executeScript("window.scrollBy(0,"+ enterIntegerValueHere + ")");	
 	}
 
-	public void scrollUpByPixel(int enterZeroValueHere, int enterNegativeValueHere) {	
-
-		executeScript("window.scrollBy("+ enterZeroValueHere +","+ enterNegativeValueHere + ")");	
+	public void scrollUpByPixel(int enterZeroValueHere, int enterIntegerValueHere) {	
+		executor.executeScript("window.scrollBy("+ enterZeroValueHere +",-"+ enterIntegerValueHere + ")");	
 	}
 	
 	public void ZoomInByPercentage(int percentageVale) {
-
-		executeScript("document.body.style.zoom='" + percentageVale + " %'");
+		executor.executeScript("document.body.style.zoom='" + percentageVale + " %'");
 	}
-
-	public String getDisplayedValue(WebElement value) {
-       
-        return (String)executeScript("return $('value').val();");
-    }
 	
+	public void setValue(WebElement webElement, String inputVale) {
+		executor.executeScript("arguments[0].setAttribute('value', '" + inputVale +"')", webElement);
+	//	executor.executeScript("document.getElementById('elementID').setAttribute('value', ' " + inputVale + "')");
+	}
+	
+	public void setValue(String uiInputFieldId, String inputVale) {
+		executor.executeScript("document.getElementById('"+ uiInputFieldId +"').setAttribute('value', '" + inputVale + "')");
+	}
+	
+	public void getPageInfo(String pageInfomation) {
+		String value = ("." + pageInfomation + ";");
+		executor.executeScript("return document'" + value + "'").toString();
+	}
+	
+	
+	/**	JavaScript locator uses document.getElementBy..('locatorName')  * in place of driver.findElement(By...("locatorName"))
+	 * 		setAttribute('inputValue')  * in place of sendKeys("inputValue")
+	 	executor.executeScript("document.getElementByLOCATORTYPE..('elementName').setAttribute('value','text@gmail.com')");
+	 */
 }
