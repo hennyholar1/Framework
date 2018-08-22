c7bc162e782c92f36ed316759500bf6550ee4901
hennyholar1 <hennyholar1@users.noreply.github.com>
2018-08-22 10:46:02 -0500

    Update PdfFileReader.java

--- POMFramework/src/main/java/com/test/automation/POMFramework/fileReader/PdfFileReader.java
+++ POMFramework/src/main/java/com/test/automation/POMFramework/fileReader/PdfFileReader.java
@@ -7,11 +7,13 @@ import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
 public class PdfFileReader {
 
-	String pathToFile;
-	PdfReader pdfReader;
+	private static final Logger log = LogFunction.getLogger(WjvPendingTransactionPO.class);
+		String pathToPdfFile;
+		String ignoreFile = "/cars-wjv-test/ignore.conf";
+		PdfReader pdfReader;
 	
 	public PdfFileReader(String pathToFile) {
-		this.pathToFile = pathToFile;
+		pathToPdfFile = pathToFile;
 		try {
 			this.pdfReader = new PdfReader(("C:\\Users\\Da Novenos\\eclipse-workspace\\POMFramework\\src\\main\\java\\com\\test\\automation\\POMFramework\\dataFile\\Geico.pdf"));
 		}
@@ -21,14 +23,30 @@ public class PdfFileReader {
 	}	
 	
 	public String retrivePdfDataStored (int pageNumber) {
-		try {
-			String data = PdfTextExtractor.getTextFromPage(pdfReader, pageNumber);
-			pdfReader.close();
-			return data;
-		} 
-		catch (IOException e) {
-			System.out.println(e.getMessage());
+		/*	https://www.youtube.com/watch?v=koMegSvZEVE
+		 *  	https://github.com/red6/pdfcompare	
+		 * 	http://www.verypdf.com/app/pdf-to-txt-converter/index.html
+		 * */
+			String pdfData = null;
+			try {
+				pdfData = PdfTextExtractor.getTextFromPage(pdfReader, pageNumber);
+				pdfReader.close();
+				return pdfData;
+			} 
+			catch (IOException e) {
+				log.info(e.getMessage());
+			}
+			return pdfData;	
 		}
-		return null;	
+	
+	public void comparePdfFiles(String expectPdfFile, String actualPdfFile, String pdfOutPutFile) {
+		//	All the files must be of .pdf format
+			try {
+			boolean result = new PdfComparator(expectPdfFile, actualPdfFile).withIgnore(ignoreFile).compare().writeTo(pdfOutPutFile);
+			log.info(result);
+			} catch (Exception e) 
+			{
+			log.info(e.getMessage());
+			}
+		}
 	}
-}
